01730954cb524e03ba771e357e568009e23d3d5f
PrabhatTheCoder <prabhat.patel7447@yahoo.com>
2025-04-10 19:56:52 +0530

    first commit

--- urlshortner/urls.py
+++ urlshortner/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
-from .views import ShortenView, RedirectView, ListUrlView, HomeView
+from .views import ShortenView, RedirectView, ListUrlView
 
 urlpatterns = [
     path('shorten/', ShortenView.as_view(), name='shorten-url'),
